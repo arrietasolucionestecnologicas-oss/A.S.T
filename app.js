55017d457a59e419270cb8f0e343b05dee31e93e
arrietasolucionestecnologicas-oss <arrietasolucionestecnologicas@gmail.com>
2026-02-06 21:55:18 -0500

    Update API_URL to new Google Apps Script URL

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // A.S.T. ADMIN FRONTEND (V24 - EDICIÓN LIVE Y TÉRMINOS)
 // ==========================================
 // *** PEGA AQUÍ LA NUEVA URL DE GOOGLE APPS SCRIPT AL IMPLEMENTAR ***
-const API_URL = "AQUI_PEGA_TU_NUEVA_URL_DEL_SCRIPT";
+const API_URL = "https://script.google.com/macros/s/AKfycbxpCp7aY4L48znjtqH_1svYzY6MjVY58bXxt3iZvyuPQwBBt0u7S32aXxxt9VVgtaHd/exec";
 const API_KEY = "AST_2025_SECURE"; 
 
 let catalog = [];
